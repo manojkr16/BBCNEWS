1b19d5a8b1c59f7854868aeff4b43fec8a9e685a
NavGurukul <manoj16@navgurukul.org>
2017-10-18 10:27:34 +0530

    BBC web

---
+++ onclick.js
@@ -0,0 +1,19 @@
+  /*document.getElementById("red-circle").onmouseover = function(){
+      document.getElementById("red-circle").style.display="none";
+  }
+  
+  
+  document.getElementById("blue-circle").onclick = function(){
+      document.getElementById("blue-circle").style.display="none";
+  }
+  
+  
+  document.getElementById("green-circle").onmouseout = function(){
+      document.getElementById("green-circle").style.display="none";
+  }
+  */
+  
+  
+    $("red-circle").hover(function(){
+         alert($("div").html());
+    });
